ec7ae7220348325a00d3479b4d8eda6249a467ad
pavelkrolevets <pavelkrolevets@gmail.com>
2023-04-17 21:51:49 +0300

    pow gost update according to 3411 hash

--- devchain/pow_gost/run_chain.sh
+++ devchain/pow_gost/run_chain.sh
@@ -6,12 +6,12 @@ tmux new -s "node1" -d &&
 tmux send-keys -t "node1" "sh run1.sh" C-m &&
 tmux detach -s "node1"
 
-tmux new -s "node2" -d &&
-tmux send-keys -t "node2" "sh run2.sh" C-m &&
-tmux detach -s "node2"
+# tmux new -s "node2" -d &&
+# tmux send-keys -t "node2" "sh run2.sh" C-m &&
+# tmux detach -s "node2"
 
-tmux new -s "node3" -d &&
-tmux send-keys -t "node3" "sh run3.sh" C-m &&
-tmux detach -s "node3"
+# tmux new -s "node3" -d &&
+# tmux send-keys -t "node3" "sh run3.sh" C-m &&
+# tmux detach -s "node3"
 
 
